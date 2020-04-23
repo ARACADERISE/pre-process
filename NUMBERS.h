3c0fafde41f00bcdcbabb4d5c8236149e90ffc1a
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-04-22 20:35:13 -0500

    Update NUMBERS.h

--- NUMBERS.h
+++ NUMBERS.h
@@ -92,24 +92,26 @@
 #define um_int64_max     18446744073709551615 // Don't really know what would need this big of a number
 
 #define typeof(value); \
-/*CHECKING NEGATIVES*/\
-if(value < 0) {\
-    if(value>=sm_int2_min&&!(value>sm_int2_max)){\
-        if(value<sm_int2_max&&value>0){\
-            printf("sm_int2_max:%d",value);\
-        } else {\
+if(!(value==0)) {\
+    if((value>=sm_int2_min&&!(value>sm_int2_max))||(!(value>um_int2_max))){\
+        if(value<=um_int2_max&&value>-1){\
+            printf("um_int2_max:%d",value);\
+        } else{\
             printf("sm_int2_min:%d",value);\
         }\
-    } else if(value>=sm_int4_min&&!(value>sm_int4_max)) {\
-        if(value<sm_int4_max&&value>0) {\
-            printf("sm_int4_max:%d",value);\
-        } else {\
-            printf("sm_int4_min:%d",value);\
+    } else if((value>=sm_int4_min&&!(value>sm_int4_max))||(!(value>um_int4_max))) {\
+        if(value<=um_int4_max&&value>-1){\
+            printf("um_int4_max:%d",value);\
+        } else{\
+            printf("um_int4_min:%d",value);\
         }\
-    } else {\
-        /* Meaning it is in the sm_int2_min/sm_int2_max range */\
-        if(value>=sm_int2_min&&!(value>sm_int2_max)){\
-            printf("sm_int2_min/sm_int2_max");\
+    } else if((value>=sm_int6_max&&!(value>sm_int4_max))||!(value>um_int6_max)){\
+        if(value<=um_int6_max&&value>-1){\
+            printf("um_int6_max:%d",value);\
+        } else{\
+            printf("sm_int6_min:%d",value);\
         }\
     }\
+} else {\
+    printf("Number %d cannot be detected",value);\
 }
