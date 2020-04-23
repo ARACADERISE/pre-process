81612a8bebc5e7cbd8af6d408d0cd6261639efaf
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-04-22 19:43:14 -0500

    Update NUMBERS.h

--- NUMBERS.h
+++ NUMBERS.h
@@ -3,60 +3,83 @@
 
 /* Max sizes
     - sm: Signed Maxed
-    - um: Unsiged Maxed
+    - um: Unsigned Maxed
+        - min ammount of um will always be zero
 */
 
-// int2: min/max
+// int2: signed: min/max
 #define sm_int2_min     -2
 #define sm_int2_max      2
-#define um_int2          4
+// unsigned: min/max
+#define um_int2_min      0
+#define um_int2_max      4
 
-// int4: min/max
+// int4: signed: min/max
 #define sm_int4_min     -4
 #define sm_int4_max      4
-#define um_int4          8
+// unsigned: min/max
+#define um_int4_min      0
+#define um_int4_ma       8
 
-// int6: min/max
+// int6: signed: min/max
 #define sm_int6_min     -62
 #define sm_int6_max      61
-#define um_int6          63
+// unsigned: min/max
+#define um_int6_min      0
+#define um_int6_max      63
 
-// int 8: min/max
+// int 8: signed: min/max
 #define sm_int8_min     -128
 #define sm_int8_max      127
-#define um_int8          255
+// unsigned: min/max
+#define um_int8_min      0
+#define um_int8_max      255
 
-// int 16: min/max
+// int 16: signed: min/max
 #define sm_int16_min    -32768
 #define sm_int16_max     32767
-#define um_int16         65535
+// unsigned: min/max
+#define um_int16_min     0
+#define um_int16_max     65535
 
-// int 20: min/max
+// int 20: signed: min/max
 #define sm_int20_min    -524289
 #define sm_int20_max     524287
-#define um_int20         1048576
+// unsigned: min/max
+#define um_int20_min     0
+#define um_int20_max     1048576
 
-// int 24: min/max
+// int 24: signed: min/max
 #define sm_int24_min    -8388608
 #define sm_int24_max     8388607
-#define um_int24         16177215
+// unsigned: min/max
+#define um_int24_min     0
+#define um_int24_max     16177215
 
-// int 28: min/max
+// int 28: signed: min/max
 #define sm_int28_min   -134217729
 #define sm_int28_max    134217728
-#define um_int28        268435456
+// unsigned: min/max
+#define um_int28_min    0
+#define um_int28_max    268435456
 
-// int 30: min/max
+// int 30: signed: min/max
 #define sm_int30_min   -536870913
 #define sm_int30_max    536870912
-#define um_int30        1073741824
+// unsigned: min/max
+#define um_int30_min    0
+#define um_int30_max    1073741824
 
-// int 32: min/max
+// int 32: signed: min/max
 #define sm_int32_min   -2147483648
 #define sm_int32_max    2147483647
-#define um_int32        4294967295
+// unsigned: min/max
+#define um_int32_min    0
+#define um_int32_max    4294967295
 
-// int 34: min/max
+// int 34: signed: min/max
 #define sm_int40_min   -549755813889
 #define sm_int40_max    549755813887
-#define um_int40        1099511627776
+// unsigned: min/max
+#define um_int40_min    0
+#define um_int40_max    1099511627776
