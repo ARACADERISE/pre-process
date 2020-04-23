59ca0d3dd074a70a036d1a7f02ced6154b254fc5
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2020-04-22 22:23:46 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include "NUMBERS.h"
 
 int main(void) {
-    int d;
+    	int d;
 
 	// Getting the number
 	printf("NUMBER: ");
 	scanf("%d",&d);
 
-    char type[50];
+    	char type[50];
 	typeof(d,&type);
 }
